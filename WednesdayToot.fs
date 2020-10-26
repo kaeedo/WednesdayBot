c678807b11386abc8aa26ce035fc820350a76ac3
Ito, Kai <kai.ito@zuehlke.com>
2020-10-26 08:28:51 +0100

    Fix parsing

--- WednesdayToot.fs
+++ WednesdayToot.fs
@@ -5,11 +5,11 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.Configuration
 open FsHttp
 open FsHttp.DslCE
-
+open System.Web
 open FSharp.Data
 
 //*/20 * * * * *
-
+//0 0 12 * * 3
 type Pleroma = JsonProvider<"./sampleStatuses.json">
 
 module TootTimer =
@@ -38,7 +38,7 @@ module TootTimer =
             |> toText
             |> Pleroma.Parse
             |> Seq.head
-            |> fun s -> s.Content
+            |> fun s -> HttpUtility.HtmlDecode(s.Content)
 
         let messagesIndex (lastMessage: string) =
             messages
@@ -55,14 +55,7 @@ module TootTimer =
             body
             ContentType "application/x-www-form-urlencoded"
             formUrlEncoded [
-                "status", sprintf """%s
-
-
-
-
----
-This is a bot. See a mistake or want to add your own language? Submit an issue or PR here: https://github.com/kaeedo/WednesdayBot
-                """ messages.[messagesIndex lastMessage]
+                "status", messages.[messagesIndex lastMessage]
             ]
         } |> ignore
 
